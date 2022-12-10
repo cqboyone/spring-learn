1881623e1359a9117fcfa7c3fbf9c639d3cc8da5
cqboyone <cqboyon@163.com>
2022-12-10 17:29:30 +0800

    增加注释

--- my-spring/src/main/java/com/vv/spring/learn/spring/MyApplicationContext.java
+++ my-spring/src/main/java/com/vv/spring/learn/spring/MyApplicationContext.java
@@ -17,6 +17,7 @@ public class MyApplicationContext {
     public MyApplicationContext(Class myConfigClass) {
         this.myConfigClass = myConfigClass;
         if (myConfigClass.isAnnotationPresent(ComponentScan.class)) {
+            //扫描包下所有class文件
             ComponentScan componentScan = (ComponentScan) myConfigClass.getAnnotation(ComponentScan.class);
             String packagePath = componentScan.value();
             ClassLoader classLoader = MyApplicationContext.class.getClassLoader();
@@ -40,19 +41,22 @@ public class MyApplicationContext {
                                 urlPath = classLoader.getResource("").getPath();
                             }
 
+                            //加载每个class文件
                             String substring = replace.substring(urlPath.length(), replace.indexOf(".class"));
                             String classPath = substring.replace("/", ".");
-
                             Class<?> aClass = classLoader.loadClass(classPath);
+
+                            //处理带有component注解的class
                             if (aClass.isAnnotationPresent(Component.class)) {
                                 Component component = aClass.getAnnotation(Component.class);
+
+                                //将class信息按 beanName -> {class;scope} 缓存
                                 String componentName = component.value();
                                 if (componentName == null || "".equals(componentName)) {
                                     String name = aClass.getName();
                                     String beanUp = name.substring(name.lastIndexOf(".") + 1);
                                     componentName = Introspector.decapitalize(beanUp);
                                 }
-
                                 String scopeType = "singe";
                                 if (aClass.isAnnotationPresent(Scope.class)) {
                                     Scope scope = aClass.getAnnotation(Scope.class);
@@ -73,7 +77,8 @@ public class MyApplicationContext {
                 }
             }
         }
-        //初始化
+
+        //初始化bean
         beanDefineMap.entrySet().forEach(entry -> {
             String key = entry.getKey();
             getBean(key);
@@ -86,6 +91,8 @@ public class MyApplicationContext {
         if (beanDefine == null) {
             throw new RuntimeException();
         }
+
+        //单例bean从缓存拿，没有就创建并放缓存。多例现创建一个。
         String scope = beanDefine.getScope();
         if ("singe".equals(scope)) {
             Object o = beanMap.get(beanName);
@@ -102,20 +109,25 @@ public class MyApplicationContext {
     public Object createBean(String beanName, BeanDefine beanDefine) {
         Class aClass = beanDefine.getaClass();
         try {
+            //空构造器构造新对象
             Object o = aClass.getConstructor().newInstance();
-            //依赖注入
+
+            //对象的属性依赖注入
             Field[] fields = aClass.getDeclaredFields();
             for (Field f : fields) {
+                //带有注入的注解进行依赖注入
                 if (f.isAnnotationPresent(Autowired.class)) {
                     f.setAccessible(true);
                     Object bean = getBean(f.getName());
                     f.set(o, bean);
                 }
             }
-            //回调
+
+            //调用回调方法
             if (o instanceof BeanNameAware) {
                 ((BeanNameAware) o).setBeanName(beanName);
             }
+            //others something like BeanNameAware
 
             beanMap.put(beanName, o);
             return o;
